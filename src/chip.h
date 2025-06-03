8ad4d242eb0fa9afef31fd54ab9813346b8a0cf4
Artur Roos <artur.roos@ktnlvr.dev>
2025-06-03 03:25:12 +0300

    Implement simple arithmetic

--- src/chip.h
+++ src/chip.h
@@ -1,38 +1,16 @@
 #ifndef __SPACED_H__CHIP__
 #define __SPACED_H__CHIP__
 
+#include <stdio.h>
 #include <string.h>
 
 #include "defs.h"
+#include "instructions.h"
 
 typedef enum chip_quota_policy_t {
   // Consume 1 per instruction
   CHIP_QUOTA_POLICY_FLAT,
 } chip_quota_policy_t;
-
-#define CHIP_OPCODE(name, hex) CHIP_OP_##name = 0x##hex
-
-// Refer to https://www.masswerk.at/6502/6502_instruction_set.html#modes
-typedef enum chip_addr_mode_t {
-  CHIP_ADDR_MODE_UNINIT,
-  CHIP_ADDR_MODE_ACCUMULATOR,
-  CHIP_ADDR_MODE_IMPLIED,
-  CHIP_ADDR_MODE_IMMEDIATE,
-  CHIP_ADDR_MODE_ZERO_PAGE,
-  CHIP_ADDR_MODE_ZERO_PAGE_X,
-  CHIP_ADDR_MODE_ZERO_PAGE_Y,
-  CHIP_ADDR_MODE_ABSOLUTE,
-  CHIP_ADDR_MODE_ABSOLUTE_X,
-  CHIP_ADDR_MODE_ABSOLUTE_Y,
-  CHIP_ADDR_MODE_INDIRECT_X,
-  CHIP_ADDR_MODE_INDIRECT_Y,
-  CHIP_ADDR_MODE_INDIRECT,
-  CHIP_ADDR_MODE_RELATIVE,
-  CHIP_ADDR_MODE_ILLEGAL,
-} chip_addr_mode_t;
-
-static bool addr_modes_lookup_table_ready = false;
-static chip_addr_mode_t addr_modes_lookup_table[0xFF] = {};
 
 typedef struct chip_t {
   // How the prices for the instructions are calculated
@@ -42,10 +20,11 @@ typedef struct chip_t {
   // The actual memory to be modified
   byte *memory;
 
-  byte a, x, y, sp, pc, p;
+  byte a, x, y, p;
+  u16 sp, pc;
 } chip_t;
 
-inline void chip_init(chip_t *self, chip_quota_policy_t policy, u32 quota,
+static void chip_init(chip_t *self, chip_quota_policy_t policy, u32 quota,
                       byte *memory) {
   memset((void *)self, 0, sizeof(chip_t));
 
@@ -54,7 +33,7 @@ inline void chip_init(chip_t *self, chip_quota_policy_t policy, u32 quota,
   self->memory = memory;
 }
 
-inline byte chip_read(chip_t *self, chip_addr_mode_t mode) {
+static byte chip_memory_read(chip_t *self, chip_addr_mode_t mode) {
   uint16_t addr;
 
   switch (mode) {
@@ -129,5 +108,43 @@ inline byte chip_read(chip_t *self, chip_addr_mode_t mode) {
     return 0;
   }
 }
+
+static u32 chip_estimate_quota(chip_t* self) {
+  byte b = self->memory[self->pc];
+  if (self->quota_policy == CHIP_QUOTA_POLICY_FLAT)
+    return 1;
+  return 0;
+}
+
+static void chip_step(chip_t *self) {
+  u32 quota = chip_estimate_quota(self);
+  if (self->quota < quota)
+    return;
+  self->quota -= quota;
+
+  byte b = self->memory[self->pc++];
+  chip_addr_mode_t mode = lookup_addr_mode(b);
+  chip_op_t op = lookup_instruction(b);
+
+  switch (op) {
+  case CHIP_OP_LDA: {
+    byte value = chip_memory_read(self, mode);
+    self->a = value;
+    break;
+  }
+  case CHIP_OP_ADC: {
+    byte value = chip_memory_read(self, mode);
+    // TODO: check for carry bit
+    self->a += value;
+    break;
+  }
+  default:
+    PANIC();
+  }
+}
+
+static void chip_dump_registers_stdout(chip_t* self) {
+  printf("A=%02X X=%02X Y=%02X\n", self->a, self->x, self->y);
+} 
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,3 +1,18 @@
+#include "chip.h"
+#include "instructions.h"
+#include <stdio.h>
+
 int main(void) {
+    init_instruction_lookups();
+
+    byte kb[0x10000] = {0xA9, 11, 0x69, 5};
+    chip_t chip;
+    chip_init(&chip, CHIP_QUOTA_POLICY_FLAT, 0xFFFF, kb);
+    
+    chip_step(&chip);
+    chip_step(&chip);
+
+    chip_dump_registers_stdout(&chip);
+
     return 0;
 }
