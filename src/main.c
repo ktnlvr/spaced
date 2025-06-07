c3bb8fe567fba9ca4066382d58cbc2cfb5b3266a
Artur Roos <artur.roos@ktnlvr.dev>
2025-06-07 20:14:23 +0300

    Spawn dump files upon completion

--- src/main.c
+++ src/main.c
@@ -13,18 +13,22 @@ int main(void) {
   byte *memory = (byte *)malloc(MEMORY_SIZE);
 
   chip_t chip;
-  chip_init(&chip, memory, 100, PROGRAM_START);
+  chip_init(&chip, memory, 44, PROGRAM_START);
 
   FILE *file = fopen("./examples/c/hello-world/main.bin", "rb");
   fread(memory + PROGRAM_START, 1, MEMORY_SIZE, file);
   fclose(file);
 
   while (chip.quota > 0) {
+    chip.quota--;
     chip_step(&chip);
     chip_dbg_dump(&chip);
     printf("Anticipated Instruction: %s\n",
            opcode_to_str(chip.memory[chip.pc]));
   }
+
+  file = fopen("./dump.bin", "wb");
+  fwrite(chip.memory, 1, MEMORY_SIZE, file);
 
   return 0;
 }
