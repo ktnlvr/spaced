d3925c12a62bbb05b8dea9caa0af163ca22c2277
Artur Roos <artur.roos@ktnlvr.dev>
2025-06-08 15:55:52 +0300

    Simplify the Fibonacci

--- examples/c/hello-world/main.c
+++ examples/c/hello-world/main.c
@@ -1,14 +1,13 @@
-static int out[15] = {};
 
 int main() {
+  int out[15] = {}, tmp;
   int a = 1, b = 1, i = 0;
   
   for (i = 0; i < 15; i++) {
     out[i] = a;
     b += a;
-    a = b ^ a;
-    b = a ^ b;
-    a = b ^ a;
+    a = b;
+    b = tmp;
   }
 
   return 0;
