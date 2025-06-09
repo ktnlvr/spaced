a4df41e834f61cbc4adc972fa9cc07bb0f3885e4
Artur Roos <artur.roos@ktnlvr.dev>
2025-06-09 16:48:10 +0300

    Fix Fibonacci

--- examples/c/hello-world/main.c
+++ examples/c/hello-world/main.c
@@ -1,10 +1,13 @@
 int main() {
-  int out[15] = {}, tmp;
-  int a = 1, b = 1, i = 0;
-  
+  char i;
+  char out[15] = {0xCC, 0xCC, 0xCC};
+  char a = 1, b = 0;
+
   for (i = 0; i < 15; i++) {
-    out[i] = a;
+    char tmp;
+    out[i] = b;
     b += a;
+    tmp = a;
     a = b;
     b = tmp;
   }
