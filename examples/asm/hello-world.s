321e7354290eab883ada0928f54620a7c1f002ac
Artur Roos <artur.roos@ktnlvr.dev>
2025-06-09 19:10:34 +0300

    Remove duplicate code from the ASM for Hello, world!

--- examples/asm/hello-world.s
+++ examples/asm/hello-world.s
@@ -33,36 +33,3 @@ done:
     rts
 
 .endproc
-; Puts "Hello, world!" at OFFSET
-OFFSET = $C800
-
-.setcpu     "6502"
-
-.forceimport    __STARTUP__
-.export     _main
-
-.segment    "CODE"
-
-.proc   _main: near
-
-.segment "RODATA"
-
-hello:
-    .asciiz "Hello, world!"
-
-.segment  "CODE"
-
-start:
-    ldx #$00
-loop:
-    lda hello,x
-    beq done
-    sta OFFSET,x
-    inx
-    jmp loop
-    
-done:
-    brk
-    rts
-
-.endproc
