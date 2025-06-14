322058b65ba2fdab9509b40312b548c7e9843f8c
Artur Roos <artur.roos@ktnlvr.dev>
2025-06-15 01:18:18 +0300

    Clean up the code for SAT

--- src/physics/sat.h
+++ src/physics/sat.h
@@ -2,19 +2,19 @@
 #define __SPACED_H__SAT__
 
 #include "../vec2.h"
+#include <alloca.h>
 
 typedef vec2 float2;
 
-static bool sat_float2_overlap(float2 a, float2 b) {
+static bool sat_float2_is_overlap(float2 a, float2 b) {
   return a.y > b.x && b.y > a.x;
 }
 
-static float2 sat_float2_combine(float2 a, float2 b) {
-  if (b.x < a.x)
-    a.x = b.x;
-  if (b.y > a.y)
-    a.y = b.y;
-  return a;
+static float2 sat_float2_overlap(float2 a, float2 b) {
+  float2 ret;
+  ret.x = (a.x > b.x) ? a.x : b.x;
+  ret.y = (a.y > b.y) ? a.y : b.y;
+  return ret;
 }
 
 static float2 sat_vec2_project(vec2 *vs, sz n, vec2 axis) {
@@ -34,32 +34,6 @@ static float2 sat_vec2_project(vec2 *vs, sz n, vec2 axis) {
   return ret;
 }
 
-static bool sat_polygons(vec2 *vs1, sz n1, vec2 *vs2, sz n2) {
-  // TODO: we assume that having the same axes is basically impossible
-  vec2 *axes = (vec2 *)calloc(sizeof(vec2), n1 + n2);
-
-  for (int i = 0; i < n1; i++) {
-    vec2 s = vec2_sub(vs1[(i + 1) % n1], vs1[i]);
-    vec2 p = vec2_perpendicular(s);
-    axes[i] = p;
-  }
-
-  for (int i = 0; i < n2; i++) {
-    vec2 s = vec2_sub(vs2[(i + 1) % n2], vs2[i]);
-    vec2 p = vec2_perpendicular(s);
-    axes[i + n1] = p;
-  }
-
-  for (sz i = 0; i < n1 + n2; i++) {
-    float2 a = sat_vec2_project(vs1, n1, axes[i]);
-    float2 b = sat_vec2_project(vs2, n2, axes[i]);
-    if (!sat_float2_overlap(a, b))
-      return false;
-  }
-
-  return true;
-}
-
 static bool sat_intersect(vec2 *vs1, sz n1, vec2 *vs2, sz n2, float2 *minimal) {
   if (minimal) {
     minimal->x = FLOAT32_INFTY;
@@ -67,7 +41,7 @@ static bool sat_intersect(vec2 *vs1, sz n1, vec2 *vs2, sz n2, float2 *minimal) {
   }
 
   // TODO: we assume that having the same axes is basically impossible
-  norm2 *axes = (norm2 *)calloc(sizeof(vec2), n1 + n2);
+  norm2 *axes = (norm2 *)alloca(sizeof(vec2) * (n1 + n2));
 
   for (int i = 0; i < n1; i++) {
     vec2 s = vec2_sub(vs1[(i + 1) % n1], vs1[i]);
@@ -88,10 +62,10 @@ static bool sat_intersect(vec2 *vs1, sz n1, vec2 *vs2, sz n2, float2 *minimal) {
     norm2 axis = axes[i];
     float2 a = sat_vec2_project(vs1, n1, axis);
     float2 b = sat_vec2_project(vs2, n2, axis);
-    is_overlap |= sat_float2_overlap(a, b);
+    is_overlap |= sat_float2_is_overlap(a, b);
 
     if (minimal) {
-      float2 overlap = sat_float2_combine(a, b);
+      float2 overlap = sat_float2_overlap(a, b);
       // NOTE(Artur): fabsf needed because the initial sentinel is 
       // two infinities the other way around
       if (overlap.y - overlap.x < fabsf(minimal->y - minimal->x))
