cccf6404fdb2eedece7e164892c0db084961c475
Artur Roos <artur.roos@ktnlvr.dev>
2025-06-30 02:11:37 +0300

    Document vectors

--- src/vec2.h
+++ src/vec2.h
@@ -5,14 +5,19 @@
 
 #include <math.h>
 
+/// @brief The vectors of this length and smaller are considered 0.
 #define VEC2_EPSILON 1e-7
 
 typedef struct {
   float x, y;
 } vec2;
 
+/// @brief Normalized vector. Does not do any work to uphold a variants,
+/// just is more type-safe. If a function returns a norm2 it must be
+/// normalized.
 typedef vec2 norm2;
 
+/// @brief `{.x = x, .y = y}`
 static vec2 vec2_new(float x, float y) {
   vec2 ret;
   ret.x = x;
@@ -20,26 +25,35 @@ static vec2 vec2_new(float x, float y) {
   return ret;
 }
 
+/// @brief `{.x = 0, .y = 0}`
 static vec2 vec2_zero() { return vec2_new(0., 0.); }
 
+/// @brief `{.x = a.x + b.x, .y = a.y + b.y}`
+/// @memberof vec2
 static vec2 vec2_add(vec2 a, vec2 b) {
   a.x += b.x;
   a.y += b.y;
   return a;
 }
 
+/// @brief `{.x = a.x - b.x, .y = a.y - b.y}`
+/// @memberof vec2
 static vec2 vec2_sub(vec2 a, vec2 b) {
   a.x -= b.x;
   a.y -= b.y;
   return a;
 }
 
+/// @brief `{.x = c * a.x, .y = c * a.y}`
+/// @memberof vec2
 static vec2 vec2_scale(vec2 a, float c) {
   a.x *= c;
   a.y *= c;
   return a;
 }
 
+/// @brief `{.x = -y, .y = x}`
+/// @memberof vec2
 static vec2 vec2_perpendicular(vec2 v) {
   vec2 ret;
   ret.x = -v.y;
@@ -47,6 +61,9 @@ static vec2 vec2_perpendicular(vec2 v) {
   return ret;
 }
 
+/// @brief Returns a vector of length 1. Returns the 0 vector for 
+/// vectors of length @ref VEC2_EPSILON and smaller.
+/// @memberof vec2
 static norm2 vec2_normalize(vec2 v) {
   norm2 ret;
   ret.x = 0.;
@@ -61,6 +78,8 @@ static norm2 vec2_normalize(vec2 v) {
   return ret;
 }
 
+/// @brief Returns a scalar product of the two vectors.
+/// @memberof vec2
 static float vec2_dot(vec2 a, vec2 b) { return a.x * b.x + a.y * b.y; }
 
 #endif
--- src/vec2i.h
+++ src/vec2i.h
@@ -8,6 +8,7 @@ typedef struct {
   i32 x, y;
 } vec2i;
 
+/// @brief `{.x = x, .y = y}`
 static vec2i vec2i_new(i32 x, i32 y) {
   vec2i ret;
   ret.x = x;
@@ -15,8 +16,10 @@ static vec2i vec2i_new(i32 x, i32 y) {
   return ret;
 }
 
+/// @brief `{.x = 0, .y = 0}`
 static vec2i vec2i_zero() { return vec2i_new(0, 0); }
 
+/// @brief `{.x = (int)v.x, .y = (int)v.y)`
 static vec2 vec2i_to_vec2(vec2i v) {
   vec2 ret;
   ret.x = v.x;
